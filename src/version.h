7d646522f5cf7c44714a80a3026c6efaf6263bf4
Aleksander Dzierżanowski <a.dzierzanowski1@gmail.com>
2022-06-24 17:11:45 +0200

    bump version

--- src/version.h
+++ src/version.h
@@ -1,9 +1,9 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VERSION "1.1.1"
+#define VERSION "1.1.2"
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 1
-#define VERSION_PATCH 1
+#define VERSION_PATCH 2
 
 #endif
